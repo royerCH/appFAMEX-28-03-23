56a1bcaf13e88dbcca282b26b9f87ade2ed12f31
miguelUlloa13 <miguel.bmangelulloa13@gmail.com>
2023-03-16 20:15:12 -0600

    Se agregan imágenes de flechas para botones

--- appFAMEX/Controller/View Configuracion/View PopUps Configuracion/PreguntasFrecuentesViewController.swift
+++ appFAMEX/Controller/View Configuracion/View PopUps Configuracion/PreguntasFrecuentesViewController.swift
@@ -11,16 +11,23 @@ class PreguntasFrecuentesViewController: UIViewController {
     
 
     @IBOutlet weak var btnCerrar: UIButton!
-    
     @IBOutlet weak var lblPreguntas: UILabel!
-  
-    
-    
     @IBOutlet weak var viewButtons: UIView!
-    
-    
     @IBOutlet weak var generales: UIButton!
+    @IBOutlet weak var lblGenerales: UILabel!
     
+    @IBOutlet weak var btnEvento: UIButton!
+    @IBOutlet weak var lblEventos: UILabel!
+    @IBOutlet weak var btnFlecha: UIImageView!
+    
+    @IBOutlet weak var btnRutas: UIButton!
+    @IBOutlet weak var lblRutas: UILabel!
+    @IBOutlet weak var textFieldGenerales: UIView!
+    
+    @IBOutlet  var arrayButton: [UIButton]!
+    
+    
+    @IBOutlet weak var viewTxt: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,13 +47,33 @@ class PreguntasFrecuentesViewController: UIViewController {
         lblPreguntas.font = .boldSystemFont(ofSize: 20)
         lblPreguntas.font = UIFont(name: "Plateia", size: 20)
         
+        lblGenerales.text = "GENERALES"
+        lblGenerales.contentMode = .center
+        lblGenerales.font = UIFont(name: "Plateia", size: 20)
+        lblGenerales.textColor = .white
+        
+        lblEventos.text = "EVENTOS"
+        lblEventos.contentMode = .center
+        lblEventos.font = UIFont(name: "Plateia", size: 20)
+        lblEventos.textColor = .white
+        
+        
+        lblRutas.text = "RUTAS"
+        lblRutas.contentMode = .center
+        lblRutas.font = UIFont(name: "Plateia", size: 20)
+        lblRutas.textColor = .white
         
         //----------------Buttons
-        generales.setTitle("GENERALES", for: .normal)
-        //generales.setTitleColor(.black, for: .normal)
         generales.setImage(UIImage(named: "imgBarraBluePopUp"), for: .normal)
+        btnEvento.setImage(UIImage(named: "imgBarraBluePopUp"), for: .normal)
+        btnRutas.setImage(UIImage(named: "imgBarraBluePopUp"), for: .normal)
         
-   
+        
+        
+        //---------------------Activate actions
+        
+        btnGenerales((Any).self)
+           
     }
     
 
@@ -55,6 +82,31 @@ class PreguntasFrecuentesViewController: UIViewController {
      
         dismiss(animated: true, completion: nil)
         print("botón Generales")
+    }
+    
+    
+   
+    func showSelectors() {
+        arrayButton.forEach{
+            button in
+            button.isHidden = !button.isHidden
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func btnGenerales(_ sender: Any) {
+        
+        
+        if generales.isHidden == false{
+            lblPreguntas.backgroundColor = .red
+        }
+        
+        
+        
+        
+        
+            
+       
     }
     
     
