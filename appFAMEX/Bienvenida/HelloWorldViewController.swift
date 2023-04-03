a60c24753eae8c08820d2643016b5ff65943a17b
miguelUlloa13 <miguel.bmangelulloa13@gmail.com>
2023-04-03 17:43:47 -0600

    creo archivo hello world

---
+++ appFAMEX/Bienvenida/HelloWorldViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HelloWorldViewController.swift
+//  appFAMEX
+//
+//  Created by Desarrollador FAMEX on 03/04/23.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+}
