da4a1d6399282ea066833b72acdb2f2943b3b599
miguelUlloa13 <miguel.bmangelulloa13@gmail.com>
2023-04-03 17:08:47 -0600

    prueba commit 1

---
+++ appFAMEX/Bienvenida/pruebaComitViewController.swift
@@ -0,0 +1,33 @@
+//
+//  pruebaComitViewController.swift
+//  appFAMEX
+//
+//  Created by Desarrollador FAMEX on 03/04/23.
+//
+
+import UIKit
+
+let texto = "Texto"
+
+class pruebaComitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
