e921eb4c609b0c8ee70c9532f3e329f6037f6e4c
miguelUlloa13 <miguel.bmangelulloa13@gmail.com>
2023-04-03 16:58:32 -0600

    qwerty

---
+++ appFAMEX/Bienvenida/qwertyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  qwertyViewController.swift
+//  appFAMEX
+//
+//  Created by Desarrollador FAMEX on 03/04/23.
+//
+
+import UIKit
+
+class qwertyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
