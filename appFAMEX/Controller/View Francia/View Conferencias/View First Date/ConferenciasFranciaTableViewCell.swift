f355567fc91bc81eee39a0831828ead18ca3fb31
miguelUlloa13 <miguel.bmangelulloa13@gmail.com>
2023-04-05 16:59:03 -0600

    En proceso estructura de datos

--- appFAMEX/Controller/View Francia/View Conferencias/View First Date/ConferenciasFranciaTableViewCell.swift
+++ appFAMEX/Controller/View Francia/View Conferencias/View First Date/ConferenciasFranciaTableViewCell.swift
@@ -23,38 +23,38 @@ class ConferenciasFranciaTableViewCell: UITableViewCell {
         super.awakeFromNib()
         
 
-        lblTitleConferencia.numberOfLines = 0
-        lblTitleConferencia.adjustsFontSizeToFitWidth = true
-        lblTitleConferencia.minimumScaleFactor = 0.5
-        lblTitleConferencia.textAlignment = .center
-        lblTitleConferencia.textColor = .white
-        
-        lblTimeConferencia.numberOfLines = 0
-        lblTimeConferencia.adjustsFontSizeToFitWidth = true
-        lblTimeConferencia.minimumScaleFactor = 0.5
-        lblTimeConferencia.textAlignment = .center
-        lblTimeConferencia.textColor = .white
-
-        
-        btnAdd.backgroundColor = .white
-        btnAdd.layer.cornerRadius = btnAdd.bounds.height / 2
-        btnAdd.clipsToBounds = true
-        
-        btnInfo.backgroundColor = .white
-        btnInfo.layer.cornerRadius = btnInfo.bounds.height / 2
-        btnInfo.clipsToBounds = true
+//        lblTitleConferencia.numberOfLines = 0
+//        lblTitleConferencia.adjustsFontSizeToFitWidth = true
+//        lblTitleConferencia.minimumScaleFactor = 0.5
+//        lblTitleConferencia.textAlignment = .center
+//        lblTitleConferencia.textColor = .black
+//
+//        lblTimeConferencia.numberOfLines = 0
+//        lblTimeConferencia.adjustsFontSizeToFitWidth = true
+//        lblTimeConferencia.minimumScaleFactor = 0.5
+//        lblTimeConferencia.textAlignment = .center
+//        lblTimeConferencia.textColor = .white
+//
+//
+//        btnAdd.backgroundColor = .red
+//        btnAdd.layer.cornerRadius = btnAdd.bounds.height / 2
+//        btnAdd.clipsToBounds = true
+//
+//        btnInfo.backgroundColor = .white
+//        btnInfo.layer.cornerRadius = btnInfo.bounds.height / 2
+//        btnInfo.clipsToBounds = true
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
-    }
-    
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+//    }
+//
 }
